6391afdd8ee92145f575466dadf6ac643848acc0
vlnk <dftvlnk@gmail.com>
2015-02-27 01:13:22 -0500

    Ajout des algorithmes génétiques

--- setcovering-core/src/metaheuristics/Genetic/AlgorithmGenetic.cpp
+++ setcovering-core/src/metaheuristics/Genetic/AlgorithmGenetic.cpp
@@ -2,12 +2,51 @@
 //  AlgorithmGenetic.cpp
 //  setcovering-core
 //
-//  Created by Valentin Laurent on 2015-02-21.
+//  Created by Valentin Laurent on 2015-02-26.
 //
 //
 
 #include "AlgorithmGenetic.h"
-
-AlgorithmGenetic::AlgorithmGenetic(Instance instance) : Algorithm(instance) {
+AlgorithmGenetic::AlgorithmGenetic(const Problem& problem, int nb_eval_max, int population_size, double mutation_probability, double born_probability, int tournament_size):
+Algorithm(problem, nb_eval_max),
+_population_size(population_size),
+_mutation_probability(mutation_probability),
+_born_probability(born_probability),
+_tournament_size(tournament_size) {
     
+    _children_population_size = (int)ceil(_born_probability*_population_size);
+}
+
+bool compareIndividual(const Individual* a, const Individual* b) {
+    return a->getObjective() < b->getObjective();
+}
+
+Solution* AlgorithmGenetic::run() {
+    std::vector<Individual*> children_population;
+    children_population.resize(_children_population_size);
+    
+    std::sort(_population.begin(), _population.end(), compareIndividual);
+    Individual* best = _population[0];
+    
+    
+    
+    return best;
+}
+
+void AlgorithmGenetic::evaluate(Solution* solution) {
+    Individual* individual = (Individual*)solution;
+    
+    std::vector<bool> chromosome = individual->getChromosome();
+    int objective_function = 0;
+    
+    for (unsigned int i=0; i < chromosome.size(); i++) {
+        if (chromosome[i]) {
+            for (unsigned int j=0; j < _problem.getCover(i).size(); j++) {
+                objective_function += _problem.getCost(_problem.getCover(i)[j]);
+            }
+        }
+    }
+    
+    _cpt_eval++;
+    individual->setObjective(objective_function);
 }
--- setcovering-core/src/metaheuristics/Genetic/AlgorithmGenetic.h
+++ setcovering-core/src/metaheuristics/Genetic/AlgorithmGenetic.h
@@ -2,7 +2,7 @@
 //  AlgorithmGenetic.h
 //  setcovering-core
 //
-//  Created by Valentin Laurent on 2015-02-21.
+//  Created by Valentin Laurent on 2015-02-26.
 //
 //
 
@@ -10,13 +10,26 @@
 #define __setcovering_core__AlgorithmGenetic__
 
 #include <stdio.h>
-#include "Algorithm.h"
+#include <cmath>
 
-class AlgorithmGenetic : Algorithm {
+#include "../Algorithm.h"
+#include "Individual.h"
+
+class AlgorithmGenetic : public Algorithm {
+private:
+    int _population_size;
+    double _mutation_probability;
+    double _born_probability;
+    int _tournament_size;
+    int _children_population_size;
+    
+    std::vector<Individual*> _population;
+    
 public:
-    AlgorithmGenetic(Instance instance);
+    AlgorithmGenetic(const Problem&, int = 10000, int = 10, double = 0.1, double = 0.1, int = 3);
     
-    
+    Solution* run();
+    void evaluate(Solution*);
 };
 
 #endif /* defined(__setcovering_core__AlgorithmGenetic__) */
